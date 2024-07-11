d656c9dbe082e8ed106609c866b6f055d76a085f
Sudhanshu Gautam <me@sudhanshug.com>
2024-07-11 20:16:06 +0530

    chore: Update serialization of object and object_changes in ContentVersion model

--- app/models/active_admin/audit/content_version.rb
+++ app/models/active_admin/audit/content_version.rb
@@ -1,11 +1,11 @@
 module ActiveAdmin
   module Audit
     class ContentVersion < PaperTrail::Version
-      serialize :object, VersionSnapshot
-      serialize :object_changes, VersionSnapshot
+      serialize :object, coder: VersionSnapshot
+      serialize :object_changes, coder: VersionSnapshot
 
-      serialize :additional_objects, VersionSnapshot
-      serialize :additional_objects_changes, VersionSnapshot
+      serialize :additional_objects, coder: VersionSnapshot
+      serialize :additional_objects_changes, coder: VersionSnapshot
 
       def self.ransackable_associations(auth_object = nil)
         []
